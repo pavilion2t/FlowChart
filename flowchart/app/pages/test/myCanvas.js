de81dcf01d64c96cb22b9c58afc43374673a440a
Judy <850832627@qq.com>
2017-06-10 12:59:04 +0800

    Update myCanvas.js
    optimize

--- flowchart/app/pages/test/myCanvas.js
+++ flowchart/app/pages/test/myCanvas.js
@@ -1,29 +1,14 @@
 import React, { PropTypes, Component } from 'react';
-import ReactDOM from 'react-dom'
-export default class myCanvas extends Component {
+import ReactDOM from 'react-dom';
 
+export default class myCanvas extends Component {
     constructor(props) {
         super(props);
-
     }
-
-    componentDidMount() {
-    }
-
-    renderLine(data){
-
-    }
-
-    draw(x,y,c,t){
-
-    }
-
-
 
     render() {
-      this.renderLine(this.props.data);
         return (
-            <canvas width="640" height="400" id="myCanvas"/>
+            <canvas width="640" height="800" id="myCanvas"/>
         );
     }
 }
