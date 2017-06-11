575ac1befff703f6828de2651176536fa0caba06
Judy <850832627@qq.com>
2017-06-11 09:55:41 +0800

    优化格式

--- flowchart/app/pages/test/Dustbin.js
+++ flowchart/app/pages/test/Dustbin.js
@@ -1,14 +1,14 @@
-import React, { PropTypes, Component } from 'react';
+import React, {Component} from 'react';
 import { DropTarget } from 'react-dnd';
 import SubBox from './SubBox';
-import MyCanvas from './MyCanvas';
+import MyCanvas from './myCanvas';
 
 const boxTarget = {
   drop(props,monitor) {
     var ClientOffset=monitor.getClientOffset();
-     {/*放置的图片的坐标：X屏幕的宽度减去左边侧边栏宽度，间距，边距*/}
-    ClientOffset.x=ClientOffset.x-230-15-30;
-    ClientOffset.y=ClientOffset.y-70-35-15-30;
+      {/*放置的图片的坐标：X屏幕的宽度减去左边侧边栏宽度，间距，边距*/}
+    ClientOffset.x=ClientOffset.x-230;
+    ClientOffset.y=ClientOffset.y-70-35-30;
     var item=monitor.getItem();
     var data = {item,offset:ClientOffset};
     return data;
@@ -24,6 +24,7 @@ function collect(connect, monitor) {
      getResult:monitor.getDropResult()
   };
 }
+
 class Dustbin extends Component {
   constructor(props){
     super(props);
@@ -34,7 +35,6 @@ class Dustbin extends Component {
   renderList(IT){
    var ds=this.state.dataSource;
     if(IT!=null){
-
       for(var i=0;i<ds.length;i++){
         if(IT.item.name==ds[i].item.name){
           ds.splice(i,1)
@@ -44,20 +44,21 @@ class Dustbin extends Component {
       ds.push(IT)
     }
     if(ds.length>0){
-      ds=ds.map((value,index)=>{
-
-        return <div key={index} style={{padding:5,position:"absolute",top:value.offset.y+30,left:value.offset.x+30,cursor:'move'}}>
+      ds = ds.map((value,index)=>{
+        {/*cursor属性定义了鼠标指针放在一个元素边界范围内时所用的光标形状*/}
+            return <div key={index}
+                    style={{
+                     position:"absolute",
+                     top:value.offset.y+30,
+                     left:value.offset.x+30,
+                     cursor:'pointer'}}>
                  <SubBox name={value.item.name}/>
                </div>
-
       })
       return ds
     }else{
       return null
     }
-  }
-  componentDidMount () {
-
   }
 
 
@@ -65,7 +66,8 @@ class Dustbin extends Component {
     const { canDrop, isOver, connectDropTarget,getItem,getResult} = this.props;
     var listItems=this.renderList(getResult)
     return connectDropTarget(
-      <div className="drag-container">
+      <div style={{backgroundColor:"#f0ffff"}}>
+        {/*画图容器*/}
         <MyCanvas ref={(ref)=>this.MyCanvas=ref} data={this.state.dataSource}/>
        {listItems}
       </div>,
