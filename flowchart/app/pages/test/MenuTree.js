d9a8297d338e50d1549cfac5b12004621cc259d1
Judy <850832627@qq.com>
2017-06-11 10:40:41 +0800

    右侧显示图片

--- flowchart/app/pages/test/MenuTree.js
+++ flowchart/app/pages/test/MenuTree.js
@@ -8,14 +8,14 @@ export default class MenuTree extends Component {
 		this.state={
 			 tree:[
 			   {flag:true,nodes: [
-						   	{name: "开始",data:{}},
-						   	{name: "循环开始",data:{}},
-				       	{name: "TCP发往短信平台",data:{}},
-			       		{name: "赋值",data:{}},
+			     {name: "开始",imguri:"./images/start.png"},
+			     {name: "循环开始",imguri:"./images/recycle.png"},
+			     {name: "TCP发往短信平台",imguri:"./images/send.png"},
+			     {name: "赋值",imguri:"./images/value.png"},
 			    ]
 			  },{flag:true,nodes: [
-								{name: "循环结束", data:{}},
-		            {name:  "结束",data:{}}
+			    {name: "循环结束", imguri:"./images/stop.png"},
+		            {name:  "结束",imguri:"./images/end.png"}
 			    ]
 			  },
 			 ]
@@ -25,13 +25,17 @@ export default class MenuTree extends Component {
 
 //这里对拖放源进行处理，tempArr是一个对象数组，有flag／nodes属性
 //如果value.nodes存在，再遍历键值对
+//<img src={require("./images/recycle.png")} style={{width:50,height:50}}/>
+
 	mapTree(){
 		var tempArr=this.state.tree;
 
 			tempArr=tempArr.map((value,index)=>{
 
 					var sub_nav=value.nodes.map((v,i)=>{
-						return(<li key={i}><Box name={v.name}/></li>)
+						return(<li key={i}>
+							<Box name={v.name} imguri={v.imguri}/>
+							</li>)
 					})
 					//这里显示
 					return(<span key={index}><span>{sub_nav}</span></span>)
