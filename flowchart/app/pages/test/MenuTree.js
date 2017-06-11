73014b7fc60472c9a487eae02ca015ed68139981
Judy <850832627@qq.com>
2017-06-11 09:53:54 +0800

    删除冗余

--- flowchart/app/pages/test/MenuTree.js
+++ flowchart/app/pages/test/MenuTree.js
@@ -7,96 +7,47 @@ export default class MenuTree extends Component {
 		super(props);
 		this.state={
 			 tree:[
-			   {
-			    title: "过程",
-          flag:true,
-			    nodes: [
+			   {flag:true,nodes: [
 						   	{name: "开始",data:{}},
 						   	{name: "循环开始",data:{}},
 				       	{name: "TCP发往短信平台",data:{}},
 			       		{name: "赋值",data:{}},
 			    ]
-			  },
-			  {
-          flag:true,
-			    title: "结束",
-			    nodes: [
-						{name: "循环结束", data:{}},
-            {name:  "结束",data:{}}
+			  },{flag:true,nodes: [
+								{name: "循环结束", data:{}},
+		            {name:  "结束",data:{}}
 			    ]
 			  },
-			]
+			 ]
 		}
 	}
 
-	setFlag(index){
-		//这个方法循环去重
-		var tempArr=this.state.tree;
-		for(var i=0;i<tempArr.length;i++){
-			if(i==index){
-				tempArr[i].flag=!tempArr[i].flag;
-			}
-		}
-		this.setState({
-			tree:tempArr
-		});
-	}
 
-//这里对拖放源进行处理，tempArr是一个对象数组，有title／flag／nodes三个属性
+//这里对拖放源进行处理，tempArr是一个对象数组，有flag／nodes属性
+//如果value.nodes存在，再遍历键值对
 	mapTree(){
 		var tempArr=this.state.tree;
-		if(tempArr == undefined || tempArr.length<1){
-			return null;
-		}else{
-			//判断tempArr不为空，就遍历键值对
+
 			tempArr=tempArr.map((value,index)=>{
-				if(value.nodes){
-          //如果value.nodes存在，再遍历键值对
+
 					var sub_nav=value.nodes.map((v,i)=>{
-						return(
-							//返回渲染一个有序列表
-							<li key={i}>
-                <Box name={v.name}/>
-							</li>
-						)
+						return(<li key={i}><Box name={v.name}/></li>)
 					})
-					return(
-						<li key={index} style={{color:"#4285f4"}}>
-
-							<span onClick={()=>this.setFlag(index)}>
-
-								<span style={{marginRight:5}}>
-								  <i className={value.flag?"iconfont icon-slide-up":"icon iconfont icon-slide-down"}></i>
-                </span>
-                {value.title}
-							</span>
-							<Collapse in={value.flag}>
-								<ul className="nav-sidebar">
-									{sub_nav}
-								</ul>
-							</Collapse>
-						</li>
-					)
-				}else{
-					return(
-						<li  key={index}>
-                <Box name={value.name}/>
-						</li>
-					)
-				}
-
-			});
+					//这里显示
+					return(<span key={index}><span>{sub_nav}</span></span>)
+			    });
 			return tempArr
-		}
+
 	}
 
-	//这是右边的拖放源，任务调度中心／业务系统／自动化编译部署／新增用户／向导
+	//这是拖放源，任务调度中心／业务系统／自动化编译部署／新增用户／向导
 	render(){
 		var treeList=this.mapTree();
 		return(
 			<div>
-          <ul className="nav-sidebar">
+          <ul>
             {treeList}
+
           </ul>
 			</div>
 		)
